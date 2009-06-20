e82807b2b28a7bf5f359e51bbaa41add56239fba
Anton Berezin <tobez@heechee.tobez.org>
2009-06-20 02:12:02 +0200

    Add the rest of the file to git control.

---
+++ www/cgi-bin/TIPP.pm
@@ -0,0 +1,12 @@
+package TIPP;
+
+our $db_name = "tipp";
+our $db_host = "192.168.42.200";
+our $db_user = "tipp";
+our $db_pass = "";
+
+our $extra_header = " - Telia - test installation";
+
+our $timezone = 'CEST';
+
+1;
